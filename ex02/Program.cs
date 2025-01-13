c6e7bbf47b96a62cb8de5101a806352ec763a408
ELIELSON FRANCISCO DA SILVA <elielson.fsilva@senacsp.edu.br>
2025-01-13 19:32:22 -0300

    aula 3

---
+++ ex02/Program.cs
@@ -0,0 +1,5 @@
+﻿// See escreva um programa que pergunte o nome do usuario, armazene esse nome em uma variavel name e apresente uma mensagem de boas-vindas exibindo o nome do usuario
+Console.WriteLine("Informe o seu nome: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Seja bem vindo(a) " + name);
---
+++ ex03/Program.cs
@@ -0,0 +1,10 @@
+﻿// ex03 escreva umprograma que solicite dois numeros inteiros para para o usuário e exiba a soma dos numeros
+Console.WriteLine("Digite o primeiro número: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo número: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o terceiro número: ");
+int number3 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o quarto número: ");
+int number4 = int.Parse(Console.ReadLine());
+Console.WriteLine("A soma dos números informados é: " + (number1 + number2 + number3 + number4));
