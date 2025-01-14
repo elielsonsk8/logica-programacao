24ed7938177d673b198bf438a864fc735502f95f
ELIELSON FRANCISCO DA SILVA <elielson.fsilva@senacsp.edu.br>
2025-01-13 22:04:21 -0300

    ex 04

---
+++ ex 04/ex 04/Program.cs
@@ -0,0 +1,27 @@
+﻿// tipo de dados
+
+string name = "naruto";
+Console.WriteLine("O nome do usuário é: " + name);
+
+int idade = 27;
+Console.WriteLine($"A idade do usuário e: {idade} anos");
+
+float carteira = 2.63F;
+
+char classificacao = 'A';
+Console.WriteLine($"O cliente é do tipo {classificacao}");
+
+bool dinheiroNaConta = true;
+
+double limeteCredito = 2000.0;
+Console.WriteLine($"Seu limite de crédito é: {limeteCredito}");
+
+string sobreNome = "uzumaki";
+
+string nomecompleto = name + sobreNome;
+Console.WriteLine(nomecompleto);
+
+double valorConta = 53.73;
+Console.WriteLine($"O valor da minha conta é: {valorConta}");
+
+double limiteConta = valorConta + limeteCredito;
